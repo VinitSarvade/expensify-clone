d5a7f2fa645047d21fd20d4d222e5983c062ea1e
Vinit Sarvade <vinit.sarvade.08@gmail.com>
2023-12-26 19:06:06 +0530

    textbox component

---
+++ src/shared/components/textbox.tsx
@@ -0,0 +1,37 @@
+import { ForwardedRef, forwardRef } from "react";
+import { TextInput, TextInputProps } from "react-native";
+import { twMerge } from "tailwind-merge";
+
+import Text from "./text";
+import Colors from "../constants/Colors";
+
+interface TextboxProps extends Omit<TextInputProps, "className"> {
+  label: string;
+  inputClassName?: string;
+  labelClassName?: string;
+}
+
+function Textbox(
+  { label, inputClassName, labelClassName, ...props }: TextboxProps,
+  ref: ForwardedRef<TextInput>,
+) {
+  return (
+    <>
+      <Text className={twMerge("text-md font-semibold", labelClassName)}>
+        {label}
+      </Text>
+      <TextInput
+        ref={ref}
+        className={twMerge(
+          "h-10 border-b border-app-border text-lg/none",
+          inputClassName,
+        )}
+        placeholderTextColor={Colors["app-text-light"]}
+        clearButtonMode="while-editing"
+        {...props}
+      />
+    </>
+  );
+}
+
+export default forwardRef(Textbox);
